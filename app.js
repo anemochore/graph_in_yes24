b3900b69dc74e3ba39a9ab44266197e83a69d0c2
anemochore <fallensky@naver.com>
2016-12-02 15:50:28 +0900

    Update app.js
    출간일 14일 전 자료부터 볼 수 있게 변경

--- app.js
+++ app.js
@@ -3,23 +3,32 @@
  */
 
 (function() {
-  const HQ_URL = "http://59.6.230.250/";
+  const HQ_URL_WITHOUT_SLASH = "http://59.6.230.250/";
+  const HQ_START_DATE = new Date(2012, 0, 3);
 
   //get yes24 code and HQ link for the title
   //let url = document.documentURI;
-  let yCode = url.slice(url.toLowerCase().indexOf("/goods/")+7);
-  if(yCode.indexOf("?") > -1) yCode = yCode.substr(0, yCode.indexOf("?"));
+  let yCode = url.slice(url.indexOf("/goods/") + 7);
+  if(yCode.indexOf("?") > -1) yCode = yCode.slice(0, yCode.indexOf("?"));
 
-  let pubDateStr = document.getElementsByClassName('pdDate')[0].innerText.slice(0, 13);
-  let pubDate = pubDateStr.slice(0,4) + pubDateStr.slice(6,8) + pubDateStr.slice(10,12);
+  const todayDate = new Date();
 
-  let todayDate = new Date();
-  let today = todayDate.getFullYear() +
-    "0".repeat(2 - (todayDate.getMonth() + 1).toString().length) + (todayDate.getMonth() + 1) +
-    "0".repeat(2 - (todayDate.getDate()).toString().length) + (todayDate.getDate());
+  const pubDateWebStr = document.getElementsByClassName('pdDate')[0].innerText.slice(0, 13);
+  let pubDate = new Date(pubDateWebStr.slice(0,4), pubDateWebStr.slice(6,8) - 1, pubDateWebStr.slice(10,12) - 14);
+  if(pubDate >= todayDate)
+    pubDate = new Date(todayDate.getFullYear(), todayDate.getMonth(), todayDate.getDate() - 14);
+  else if(pubDate < HQ_START_DATE)
+    pubDate = HQ_START_DATE;
 
-  let LINK = HQ_URL + 'showTrend.php?sd=' + pubDate + '&ed=' + today + '&prodList=' + yCode;
+  const startDateStr = dateToYYYYMMDD(pubDate);
+  const endDateStr = dateToYYYYMMDD(todayDate);
+  const LINK = `${HQ_URL_WITHOUT_SLASH}/showTrend.php?sd=${startDateStr}&ed=${endDateStr}&prodList=${yCode}`;
 
+  function dateToYYYYMMDD(date) {
+    return date.getFullYear() +
+      "0".repeat(2 - (date.getMonth() + 1).toString().length) + (date.getMonth() + 1) +
+      "0".repeat(2 - (date.getDate()).toString().length) + (date.getDate());
+  }
 
   //add a new layer and show it :)
   //from http://stackoverflow.com/a/19065024/6153990
@@ -37,8 +46,9 @@
     overflow = 'auto';
   }
   div.id = 'fy_div';
-  div.innerHTML = '<a href="' + LINK + '" target="_blank">[more]</a> ' +
-    '<a href="javascript:void(0)" onclick="document.body.removeChild(document.getElementById(' + "'fy_div'" + '));">[close]</a><br/>' +
-    '<iframe src="' + LINK + '" width="100%" style="height: 60vh;"></iframe>';
+  div.innerHTML = `<a href="${LINK}" target="_blank">[more]</a> 
+<a href="javascript:void(0)" onclick="document.body.removeChild(document.getElementById('${div.id}'));">[close]</a><br/>
+<iframe src="${LINK}" width="100%" style="height: 60vh;"></iframe>`;
+
   document.body.appendChild(div);
 }());
